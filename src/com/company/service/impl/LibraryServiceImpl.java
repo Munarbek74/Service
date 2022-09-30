34d21e6903ecd7a3864bdf40a23d04b9c1eafa07
Munarbek74 <munarbekshorokhov@gmail.com>
2022-09-30 19:42:01 +0600

    1 commit

--- src/com/company/service/impl/LibraryServiceImpl.java
+++ src/com/company/service/impl/LibraryServiceImpl.java
@@ -69,41 +69,13 @@ public class LibraryServiceImpl implements LibraryService {
              dao.getLibrary().getLibraryMembers().stream().filter(x -> x.getMemberId() == memberId).toList().get(0).setCurrentlyReading(b);
             dao.getLibrary().getBooks().stream().filter(x -> x.getBookId() == bookId).toList().get(0).setCurrentHolder(m);
         } else {
-            throw new Exception(b.getCurrentHolder().getName() + " is reading this book");
+            try {
+                throw new Exception(b.getCurrentHolder().getName() + " is reading this book");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
-//    @Override
-//    public void addBookToMember(Long memberId, long bookId) {
-//        Book b = findLibraryBookById(bookId);
-//        LibraryMember m = findLibraryMemberById(memberId);
-//        Stream<Object> a = dao.getLibrary().getBooks().stream().map(x -> x.getCurrentHolder().getMemberId());
-//        Stream<Object> c = dao.getLibrary().getLibraryMembers().stream().map(x -> x.getCurrentlyReading().getBookId());
-////        dao.getLibrary().getLibraryMembers().stream().filter(x -> x.getMemberId() == memberId).toList().get(0).setCurrentlyReading(b);
-//        dao.getLibrary().getBooks().stream().filter(x -> x.getBookId() == bookId).toList().get(0).setCurrentHolder(m);
-//        System.out.println(dao.getLibrary().getBooks().stream().map(x -> x.getCurrentHolder().getMemberId()).collect(Collectors.toList()));
-//
-//        dao.getLibrary().getBooks().stream().map(x -> {
-//            if (x.getCurrentHolder().getMemberId() != memberId) {
-//                dao.getLibrary().getLibraryMembers().stream().filter(y -> y.getMemberId() == memberId).toList().get(0).setCurrentlyReading(b);
-//            }
-//            return x;
-//
-//        }).collect(Collectors.toList());
-//        System.out.println(dao);
-//    }
-//
-//
-//        if (c != null) {
-//        if (dao.getLibrary().getLibraryMembers().get(0).getCurrentlyReading().getBookId() == bookId) {
-//                System.out.println("Книга не свободна");
-//            } else {
-//            dao.getLibrary().getLibraryMembers().stream().filter(x -> x.getMemberId() == memberId).toList().get(0).setCurrentlyReading(b);
-//            }
-//        } else {
-//            dao.getLibrary().getLibraryMembers().stream().filter(x -> x.getMemberId() == memberId).toList().get(0).setCurrentlyReading(b);
-//        }
-//        }
-//    }
     @Override
     public void removeBookFromReading(Long memberId, long bookId){
         Book b = findLibraryBookById(bookId);
